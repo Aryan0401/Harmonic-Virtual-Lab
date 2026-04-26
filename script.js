3e28f57969b94a09080ab722c16b0f99d6d9af5c
Aryan Bansode <www.aryanbansode2007@gmail.com>
2026-04-26 18:53:29 +0530

    0.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculate()
 {
-    let n = 6;
+    let n = parseInt(document.getElementById("n").value);
     let asum = 0;
     let a0 = 0;
     let bsum = 0;
@@ -18,26 +18,34 @@ function calculate()
 
    
     let a0_final = (2 / n) * a0;
-    let a1 = (2 / n) * asum;
-    let b1 = (2 / n) * bsum;
+    let a_1 = (2 / n) * asum;
+    let b_1 = (2 / n) * bsum;
+
+    let a1 = a_1.toFixed(4);
+    let b1 = b_1.toFixed(4);
+    let a0_display = a0_final.toFixed(4);
 
     document.getElementById("result").innerHTML =
-        `f(x) = ${a0_final / 2} + ${a1}cos(x) + ${b1}sin(x)`;
+        `f(x) = ${a0_display / 2} + ${a1}cos(x) + ${b1}sin(x)`;
 }
 
 
 
 
-// function generateTable()
-// {
-//     let n = parseInt(document.getElementById("n").value);
-//     let table = document.getElementById("table-js");
-//     table.innerHTML = "";
-//     for (let i = 1; i <= n; i++) {
-//         let row = table.insertRow();
-//         let cell1 = row.insertCell(0);
-//         let cell2 = row.insertCell(1);
-//         cell1.innerHTML = `<input type="number" id="x${i}">`;
-//         cell2.innerHTML = `<input type="number" id="y${i}">`;
-//     }
-// }
+function generateTable()
+{
+    let n = parseInt(document.getElementById("n").value);
+    let table = document.getElementById("table-js");
+    table.innerHTML = 
+    `   <tr>
+            <th>X</th>
+            <th>Y</th>
+        </tr>`;
+    for (let i = 1; i <= n; i++) {
+        let row = table.insertRow();
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        cell1.innerHTML = `<input type="number" id="x${i}">`;
+        cell2.innerHTML = `<input type="number" id="y${i}">`;
+    }
+}
